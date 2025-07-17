6b97528939bb2ed67f4641c4428d57b2b0478786
user <lizosazi@gmail.com>
2025-07-17 21:13:07 +0200

    allowed CORS to formSchema

--- backend/app/routes/students.py
+++ backend/app/routes/students.py
@@ -54,6 +54,7 @@ def list_students():
     }), 200
 
 @students_bp.route("/form_schema", methods=["GET"])
+@cross_origin(origins="http://localhost:3000", supports_credentials=True)
 @jwt_required()
 def student_form_schema():
     schema = generate_schema_from_model(Student)
