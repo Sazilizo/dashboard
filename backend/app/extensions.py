df241c569700a9e280c833e30ea156d0bb77e84d
user <lizosazi@gmail.com>
2025-07-18 02:21:21 +0200

    increased limit for  testing

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -13,6 +13,6 @@ migrate= Migrate()
 limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=os.getenv("REDIS_URL"),
-    default_limits=["2000 per day", "300 per hour"],
+    default_limits=["20000 per day", "1000 per hour"],
     on_breach=log_rate_limit_violation
 )
