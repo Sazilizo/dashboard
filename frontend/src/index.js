adc145f9c019068e91acf9c5c250d3d0f9f31a61
unknown <lizosazi@gmail.com>
2025-08-29 18:20:51 +0200

    debugging not found supabaseUrl

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 
+
+console.log("supabaseUrl",process.env.REACT_APP_SUPABASE_URL)
+console.log("supabaseAnonKey",process.env.REACT_APP_SUPABASE_ANON_KEY)
+
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
