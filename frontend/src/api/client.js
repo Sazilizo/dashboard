43cdb619dc77f7f8c8a70f934e46b662a0f47308
unknown <lizosazi@gmail.com>
2025-08-29 18:27:42 +0200

    debugging not found supabaseUrl in app.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,14 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Grab env variables injected by Webpack
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error(
-//     "Supabase environment variables are missing! Check your .env file."
-//   );
-// }
 
 const api = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./app";
 
 
-console.log("supabaseUrl",process.env.REACT_APP_SUPABASE_URL)
-console.log("supabaseAnonKey",process.env.REACT_APP_SUPABASE_ANON_KEY)
-
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
