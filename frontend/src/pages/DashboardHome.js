88396343c982c36e100c8994b2fe71ca54f61af3
user <lizosazi@gmail.com>
2025-09-29 07:47:40 +0200

    removed data in dashboardHome

--- frontend/src/pages/DashboardHome.js
+++ frontend/src/pages/DashboardHome.js
@@ -9,19 +9,19 @@ import { useSupabaseWorkers } from "../hooks/useSupabaseWorkers";
 import SkeletonList from "../components/widgets/SkeletonList";
 import ListItems from "../components/widgets/ListItems";
 export default function DashboardHome() {
-  const {user} = useAuth();
-  const {schools} = useAuth();
-  const {filters, setFilters} = useFilters()
+  // const {user} = useAuth();
+  // const {schools} = useAuth();
+  // const {filters, setFilters} = useFilters()
 
-  const isAllSchoolRole = ["superuser", "admin", "hr", "viewer"].includes(user?.profile?.roles.name);
-    const { workers, loading, error } = useSupabaseWorkers({
-      school_id: isAllSchoolRole ? schools.map((s) => s.id) : [user?.profile?.school_id],
-    });
+  // const isAllSchoolRole = ["superuser", "admin", "hr", "viewer"].includes(user?.profile?.roles.name);
+  //   const { workers, loading, error } = useSupabaseWorkers({
+  //     school_id: isAllSchoolRole ? schools.map((s) => s.id) : [user?.profile?.school_id],
+  //   });
 
   return (
     <div>
       <h2>School Overview</h2>
-      <div className="app-list-filters">
+      {/* <div className="app-list-filters">
           <FiltersPanel
             user={user}
             schools={schools}
@@ -36,7 +36,7 @@ export default function DashboardHome() {
           {loading && <SkeletonList count={10} />}
           {!loading && error && <div style={{color:"red"}}>{error.message || error}</div>}
           {!loading && !error && <ListItems schools={schools} />}
-      </div>
+      </div> */}
     </div>
   );
 }
